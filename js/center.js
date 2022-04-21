a0687b97b8b2c5efac97bde7e026c6fc2b33812a
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2022-04-21 16:45:53 +0900

    Add files via upload

--- js/center.js
+++ js/center.js
@@ -29,7 +29,8 @@ var value = new Vue({
         circleR:0,
         fontH:12,
         dist:0,
-        animTime:0
+        animTime:0,
+        aft1:''
     },
     mounted:function(){
         document.getElementById("speed").addEventListener('input', this.view_control, false)
@@ -39,36 +40,80 @@ var value = new Vue({
 
     },
     methods:{
+
+        remove_span_1:function(){
+            var elems=app.querySelectorAll(".span_2")
+            let int_loop2=0
+            for(int_loop2;int_loop2<elems.length;int_loop2++){
+                //elems[int_loop2].classList.remove("span_1")
+                elems[int_loop2].style.transform='translate(0px, 0px) rotateX(90deg) rotateY(0deg)'
+                //elems[int_loop2].style.animationDuration = '0s'
+                elems[int_loop2].style.animationName="x"
+
+                void elems[int_loop2].offsetWidth;
+            }
+            alert("cf")
+        },
+        add_span_1:function(){
+            var elems=app.querySelectorAll(".span_2")
+            let int_loop2=0
+            for(int_loop2;int_loop2<elems.length;int_loop2++){
+                console.log(elems[int_loop2].style)
+
+                elems[int_loop2].classList.add("span_1")
+                elems[int_loop2].style.animationName="opacityAnim"
+                //console.log(elems[int_loop2].classList)
+            }
+
+        },
+
+
+
         view_control:function(){
 
 
            //console.log(this.int_speed*100)
             if(this.int_speed>0){
-                //fly wheel
-                this.animTime=parseInt(60/(this.int_speed/2)).toFixed(3)
+                //***************************** /fly wheel *****************************
+                this.obj_1.innerHTML=this.aft1
 
-                //console.log(this.animTime)
-    
+
+
+                this.animTime=(60/(this.int_speed)).toFixed(2)
+                
                 let nm_1=0
                 let radX=0
                 let radY=0
                 let txtcnt_2=this.textcnt
-                let dist_2=this.dist
+                let dist_2=this.circleR - this.fontH
                 let anitime_2=this.animTime
-    
-    
-    
-    
-                app.querySelectorAll(".span_1").forEach(function(val,index,ar){
-                    nm_1 = index+1
+
+                //console.log(this.int_speed +" "+anitime_2)
+                this.remove_span_1()
+                this.add_span_1()
+
+                var elems=app.querySelectorAll(".span_1")
+
+
+
+                let int_loop2=0
+                for(int_loop2;int_loop2<elems.length;int_loop2++){
+                    nm_1 = int_loop2+1
+
+                    //console.log(elems[int_loop2].style.transformOrigin)
+
                     radX = Math.sin(360 / txtcnt_2 * nm_1 * (Math.PI / 180))
                     radY = Math.sin((90 - (360 / txtcnt_2 * nm_1)) * (Math.PI / 180))
-                    val.style.animationDelay = anitime_2 / txtcnt_2 * nm_1 + 's'
-                    val.style.transform='translate(' + dist_2 * radX + 'px, ' + -(dist_2 * radY) + 'px) rotateX(90deg) rotateY(' + 360 / txtcnt_2 * nm_1 + 'deg)'
-    
-                });
-                app.querySelectorAll(".text")[0].style.animationDuration=anitime_2+'s'
-    
+
+
+                    elems[int_loop2].style.transform='translate(' + dist_2 * radX + 'px, ' + -(dist_2 * radY) + 'px) rotateX(90deg) rotateY(' + 360 / txtcnt_2 * nm_1 + 'deg)'
+                    elems[int_loop2].style.animationDelay = anitime_2 / txtcnt_2 * nm_1 + 's'
+                    elems[int_loop2].style.animationDuration = anitime_2+ 's'
+                }
+
+                /*app.querySelectorAll(".text")[0].style.animationDuration=anitime_2+'s'*/
+                //***************************** /fly wheel *****************************
+                document.getElementById("txt_1").style.animationDuration=anitime_2+'s'
     
     
             }
@@ -108,27 +153,27 @@ var value = new Vue({
 
             if(this.exec1==0){
 
-                //fly wheel
+                //***************************** /fly wheel *****************************
                 this.obj_1=document.getElementById('txt_1')
                 this.str_txt_1=this.obj_1.innerText
                 this.ar_txt_1=this.str_txt_1.split("")
                 let after = '';
                 this.ar_txt_1.forEach(function(val,index,ar){
-                    after += "<span class='span_1'>" + val + "</span>";
+                    after += "<span class='span_1 span_2'>" + val + "</span>";
                 });
-                this.obj_1.innerHTML=after
+                this.aft1=after
                 this.textcnt = this.ar_txt_1.length
                 let rect_1=document.getElementsByClassName("circle")[0].getBoundingClientRect()
-                this.circleR = (rect_1.height) / 2
+                this.circleR = (rect_1.width) / 2
                 let rect_2=document.getElementsByClassName("inner")[0].getBoundingClientRect()
                 this.fontH = rect_2.height
-                this.dist = this.circleR - this.fontH;
+                this.dist = this.circleR - this.fontH
     
                 //let elem_1=document.getElementsByClassName("text")[0]
                 //let hhh=window.getComputedStyle(elem_1)
                 //let shhh=hhh.getPropertyValue('animation-duration')
                 //this.animTime = shhh.slice(0, -1);
-    
+                //***************************** /fly wheel *****************************
     
 
 
